f9f8e653076f3a1785a51db82d2d49fc2b1b7264
1598008146 <69795323+1598008146@users.noreply.github.com>
2025-11-27 09:21:03 +0800

    Update csbot.py

--- csbot.py
+++ csbot.py
@@ -167,7 +167,9 @@ if __name__ == "__main__":
     bot.set_webhook(url=WEBHOOK_URL)
     
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
+
 
 
 
